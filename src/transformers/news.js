475bae5a752db9f6dcceba011cf081bdbc309f17
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-11-03 15:53:02 +0700

    UPDATE TRANSFORMER

--- src/transformers/news.js
+++ src/transformers/news.js
@@ -6,10 +6,8 @@ export const newsTransformers = (data) => {
     slug: data.slug,
     title: data.title,
     image_url: data.image_url,
-    createdAt: {
-      human: human(data.createdAt, { languages: "id" }),
-      tanggal: new Intl.DateTimeFormat("id-ID", { day: "2-digit", month: "short", year: "numeric" }).format(new Date()), // e.g. "15 Jun 2022"
-    },
+    human: human(data.createdAt, { languages: "id" }),
+    tanggal: new Intl.DateTimeFormat("id-ID", { day: "2-digit", month: "short", year: "numeric" }).format(new Date()), // e.g. "15 Jun 2022"
   };
 };
 
