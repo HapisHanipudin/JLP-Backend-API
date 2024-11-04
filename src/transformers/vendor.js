715f16f8fc06ac67707cd8113c7422044fb5a37b
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-11-04 20:39:25 +0700

    add reviws amount

--- src/transformers/vendor.js
+++ src/transformers/vendor.js
@@ -13,6 +13,18 @@ export const vendorDetailTransformer = (data) => {
 export const vendorTransformer = (data) => {
   const rating = data.reviews.length > 0 ? parseFloat((data.reviews.reduce((acc, curr) => acc + curr.rating, 0) / data.reviews.length).toFixed(2)) : 0;
 
+  const reviewCounts = data.reviews.reduce(
+    (acc, curr) => {
+      if (curr.rating === 5) acc.five += 1;
+      if (curr.rating === 4) acc.four += 1;
+      if (curr.rating === 3) acc.three += 1;
+      if (curr.rating === 2) acc.two += 1;
+      if (curr.rating === 1) acc.one += 1;
+      return acc;
+    },
+    { five: 0, four: 0, three: 0, two: 0, one: 0 }
+  );
+
   return {
     id: data.id,
     slug: data.slug,
@@ -25,6 +37,7 @@ export const vendorTransformer = (data) => {
     closingHours: data.closingHours,
     mapsUrl: data.gmapsUrl,
     icon: data.iconUrl,
+    reviews: reviewCounts,
   };
 };
 
