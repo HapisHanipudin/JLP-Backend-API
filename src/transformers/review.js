5a558d1bd943413d27846ac0b73bbaeb621986c5
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-11-13 14:31:41 +0700

    added cart transformer

--- src/transformers/review.js
+++ src/transformers/review.js
@@ -1,5 +1,7 @@
 import human from "human-time";
 import { userTransformer } from "./user.js";
+import cart from "../controllers/cart.js";
+import { productTransformer, vendorTransformer } from "./vendor.js";
 
 export const reviewTransformer = (review) => {
   return {
@@ -8,5 +10,16 @@ export const reviewTransformer = (review) => {
     comment: review.comment,
     created: human(review.createdAt),
     user: userTransformer(review.user),
+  };
+};
+
+export const cartTransformer = (cart) => {
+  return {
+    id: cart.id,
+    productId: cart.productId,
+    quantity: cart.quantity,
+    note: cart.note,
+    product: productTransformer(cart.product),
+    vendor: vendorTransformer(cart.product.vendor),
   };
 };
