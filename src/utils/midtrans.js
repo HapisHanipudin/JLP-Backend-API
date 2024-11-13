1dcd0a6a7a91f45db140fa3c4882d1c818026159
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-11-13 20:39:59 +0700

    change midtrans key to env

--- src/utils/midtrans.js
+++ src/utils/midtrans.js
@@ -1,7 +1,8 @@
 import midtransClient from "midtrans-client";
+
 export const snap = new midtransClient.Snap({
   // Set to true if you want Production Environment (accept real transaction).
   isProduction: false,
-  serverKey: "SB-Mid-server-I5EQSLwuG3BsEPgmdNkZExOY",
-  clientKey: "SB-Mid-client-UM05dNq-Mq-rYQdQ",
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
