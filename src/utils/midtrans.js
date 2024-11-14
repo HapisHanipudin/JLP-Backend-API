54ec87e49a94ffd4923e49567171baa44dcc4c28
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-11-14 11:02:28 +0700

    updated key

--- src/utils/midtrans.js
+++ src/utils/midtrans.js
@@ -3,6 +3,6 @@ import midtransClient from "midtrans-client";
 export const snap = new midtransClient.Snap({
   // Set to true if you want Production Environment (accept real transaction).
   isProduction: false,
-  serverKey: process.env.MIDTRANS_SERVER_KEY,
-  clientKey: process.env.MIDTRANS_CLIENT_KEY,
+  serverKey: "SB-Mid-server-Y1sjkAeW-fxXhzgJqg7jswRD",
+  clientKey: "SB-Mid-client-9gOtmgGB6g_eNC3_",
 });
