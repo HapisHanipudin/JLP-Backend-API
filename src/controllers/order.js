c61bdabadee28571ebe676c8c29b864f73f002a6
Hapis Hanipudin <hapis.hanipuddin@gmail.com>
2024-11-14 11:10:33 +0700

    auu

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -64,7 +64,7 @@ export default {
       });
 
       const paymentInformation = {
-        token: transaction.token,
+        snapToken: transaction.token,
         redirect_url: transaction.redirect_url,
       };
 
